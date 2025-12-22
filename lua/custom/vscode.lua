79f8378959f35950e1fe0ba3442e458450ee2432
fatpandac <i@fatpandac.com>
2025-12-22 17:34:52 +0800

    feat: support vscode-neovim

---
+++ lua/custom/vscode.lua
@@ -0,0 +1,25 @@
+require("custom.base")
+
+vim.g.mapleader = " "
+vim.keymap.set('n', 'j', ":call VSCodeCall('cursorDown')<CR>", { noremap = false, silent = true })
+vim.keymap.set("n", "k", ":call VSCodeCall('cursorUp')<CR>", { noremap = false, silent = true })
+vim.keymap.set("n", "<leader>x", ":call VSCodeNotify('workbench.action.closeActiveEditor')<CR>", { silent = true })
+vim.keymap.set("n", "<leader>X", ":call VSCodeNotify('workbench.action.closeOtherEditors')<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ff", ":call VSCodeNotify('fzfsearch.search.file.toggle')<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fw", ":call VSCodeNotify('fzfsearch.search.content.toggle')<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fg", ":call VSCodeNotify('fzfsearch.search.repo.toggle')<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gt", ":call VSCodeNotify('fzfsearch.search.gitStatus.toggle')<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fm", ":call VSCodeNotify('editor.action.formatDocument')<CR>", { silent = true })
+vim.keymap.set("n", "za", ":call VSCodeNotify('editor.toggleFold')<CR>", { silent = true })
+vim.keymap.set("n", "]d", ":call VSCodeNotify('editor.action.marker.nextInFiles')<CR>", { silent = true })
+vim.keymap.set("n", "[d", ":call VSCodeNotify('editor.action.marker.prevInFiles')<CR>", { silent = true })
+vim.keymap.set("n", "gd", ":call VSCodeNotify('editor.action.revealDefinition')<CR>", { silent = true })
+vim.keymap.set("n", "gr", ":call VSCodeNotify('editor.action.goToReferences')<CR>", { silent = true })
+vim.keymap.set("n", "gg", ":call VSCodeNotify('list.focusFirst')<CR>", { silent = true })
+vim.keymap.set("n", "gg", ":call VSCodeNotify('cursorTop')<CR>", { silent = true })
+vim.keymap.set("n", "ra", ":call VSCodeNotify('editor.action.rename')<CR>", { silent = true })
+vim.keymap.set("n", "cl", function()
+  local line = vim.fn.line "."
+  local file = vim.fn.expand "%"
+  vim.fn.setreg("+", file .. ":" .. line)
+end, { silent = true })
